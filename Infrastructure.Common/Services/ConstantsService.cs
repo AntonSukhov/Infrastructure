d9c13af36b68e8bb1e99ecaef0d3834babdc999e
Сухов Антон Александрович <sukhovaa@gnivc.ru>
2025-06-04 15:41:17 +0300

    Добавлен сервис ConstantsService в Infrastructure.Common

---
+++ Infrastructure.Common/Services/ConstantsService.cs
@@ -0,0 +1,13 @@
+namespace Infrastructure.Common.Services;
+
+/// <summary>
+/// Сервис для сохранения и получения постоянных значений.
+/// </summary>
+public static class ConstantsService
+{
+    #region Константы
+
+    public const string JsonDeserializationErrorMessage = "Ошибка десериализации JSON: результат десериализации равен null.";
+
+    #endregion
+}
