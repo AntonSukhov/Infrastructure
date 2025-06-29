b4827bf7ce2fa6300620c0103754243ca5489dac
Anton Sukhov <RealComrade2011@yandex.ru>
2025-06-29 16:34:03 +0300

    Убрал лишний метод тестирования

--- Infrastructure.Security.Tests/CryptographyServiceTests/EncryptTests.cs
+++ Infrastructure.Security.Tests/CryptographyServiceTests/EncryptTests.cs
@@ -41,46 +41,5 @@ public class EncryptTests
 
         exception.Match(p => p.Any(p1 => p1 is ArgumentException || p1 is ArgumentNullException || p1 is CryptographicException));
     }
-
-    [Fact]
-    public void Method()
-    {
-        var text = "Hello World!!!";
-        var message = string.Empty;
-        var secureKey32BitSize = 32;
-        var secureKey64BitSize = 64;
-
-        var secureKey32Bit = CryptographyService.GenerateSecureKey(secureKey32BitSize);
-        var secureKey64Bit = CryptographyService.GenerateSecureKey(secureKey64BitSize);
-        var secureKey64BitLength = Convert.ToBase64String(new byte[secureKey64BitSize]).Length;
-
-        if (!string.IsNullOrWhiteSpace(secureKey64Bit) &&
-            secureKey64Bit.Length == secureKey64BitLength)
-        {
-            message = "The generate secure key was successful!";
-        }
-        else
-        {
-            message = "The generate secure key was unsuccessful!";
-        }
-
-        Console.WriteLine(message);
-
-        var encryptText = CryptographyService.Encrypt(text, secureKey32Bit);
-        var decryptText = CryptographyService.Decrypt(encryptText, secureKey32Bit);
-
-        if (!string.IsNullOrWhiteSpace(encryptText) &&
-            text == decryptText)
-        {
-            message = "The encrypt and decrypt was successful!";
-        }
-        else
-        {
-            message = "The encrypt and decrypt was unsuccessful!";
-        }
-        
-        Console.WriteLine(message);
-    }
-
     #endregion
 }
